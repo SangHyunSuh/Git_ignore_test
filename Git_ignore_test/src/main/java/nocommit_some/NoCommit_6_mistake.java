59044b0e370cf26e70630b4fed93b03ca910094c
sanghyun0 <tkdgus078@naver.com>
2023-04-18 16:27:47 +0900

    6번 커밋함

--- Git_ignore_test/src/main/java/nocommit_some/NoCommit_6_mistake.java
+++ Git_ignore_test/src/main/java/nocommit_some/NoCommit_6_mistake.java
@@ -12,7 +12,7 @@ public class NoCommit_6_mistake {
 		  5. NoCommit_6_mistake.java 파일을  ignore 되도록 하기 위한 무식한 방법은  
 		     NoCommit_6_mistake.java 파일의 내용을 메모장으로 백업받은 후
 		     NoCommit_6_mistake.java 파일을 삭제한 후 push 하고 
-		          다시 NoCommit_6_mistake.java 파일을 재생성해서 백업해둔 메모장의 내용으로 덮어씌우면 됩니다. 
+		     다시 NoCommit_6_mistake.java 파일을 재생성해서 백업해둔 메모장의 내용으로 덮어씌우면 됩니다. 
 	*/	
 		System.out.println("=== NoCommit_6_mistake.java 파일입니다. ==="); 
 	}
